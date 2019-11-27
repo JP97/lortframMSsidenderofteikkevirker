5ef044c7691b530b8b47ff81e65f569ab616f7f4
Julian Petersen <juli11c0@edu.ucl.dk>
2019-11-27 17:16:58 +0100

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using lortframMSsidenderofteikkevirker.Areas.Identity.Data;
+using lortframMSsidenderofteikkevirker.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(lortframMSsidenderofteikkevirker.Areas.Identity.IdentityHostingStartup))]
+namespace lortframMSsidenderofteikkevirker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<lortframMSsidenderofteikkevirkerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("lortframMSsidenderofteikkevirkerContextConnection")));
+
+                services.AddDefaultIdentity<lortframMSsidenderofteikkevirkerUser>()
+                    .AddEntityFrameworkStores<lortframMSsidenderofteikkevirkerContext>();
+            });
+        }
+    }
+}
